a0af4bb2fc4abf22575bff87b00fd1eba9e5c5d3
Kolya-Harutyunyan <haryan.geko@gmail.com>
2021-11-11 11:05:59 +0400

    change product dialog

--- src/components/dialogs/ProductsDialog.js
+++ src/components/dialogs/ProductsDialog.js
@@ -3,6 +3,11 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { selectCartItems, selectTotalAmount } from '../../features/shoppingCartItems/shoppingCartItemsSlice';
 import CartItem from '../CartItem';
+import Login from '../modals/login';
+import SignUp from '../modals/signUp';
+import ForgotPass from '../modals/forgotPass';
+import NewPass from '../modals/resetPass';
+import VerifyCode from '../modals/verifyCode';
 import { useHistory } from 'react-router';
 import Alert from '@material-ui/lab/Alert';
 import '../../scss/ProductsDialog.scss'
@@ -10,7 +15,11 @@ import '../../scss/ProductsDialog.scss'
 
 function ProductsDialog() {
   const shoppingCartItems = useSelector(selectCartItems);
-  const token = localStorage.getItem('token')
+  let [token, setToken] = useState(
+    window.localStorage.getItem('token')
+      ? window.localStorage.getItem('token')
+      : null
+  )
   const products = useSelector(store => {
     return store.shoppingCartItem.cartItems
   })
@@ -24,18 +33,75 @@ function ProductsDialog() {
     return store.customer.delivery_addresses?.length || null
   });
 
-  const [alert,setAlert] = useState(false)
+  const [alert,setAlert] = useState(false);
+  const [loginActiv, setLoginActiv] = useState(false);
+  const [signUpActiv, setSignUpActiv] = useState(false);
+  const [forgotActive, setForgotActive] = useState(false);
+  const [verifyCodeActive, setVerifyCodeActive] = useState(false);
+  const [newPassActive, setNewPassActive] = useState(false);
+  const [res, setRes] = useState({});
 
   const history = useHistory()
 
-  function checkoutPageRedirect() {
-    if (!delAddresses || !verified) {
-      setAlert(true)
-    } 
-   else {
-    setAlert(false)
-    history.push('/checkout')
+  const loginOpen = () => {
+    setLoginActiv(!loginActiv)
+    setSignUpActiv(false)
+    setForgotActive(false)
+    setVerifyCodeActive(false)
   }
+  const SignUpOpen = () => {
+    setSignUpActiv(!signUpActiv)
+    setLoginActiv(false)
+    setForgotActive(false)
+    setVerifyCodeActive(false)
+  }
+
+  const ForgotPassOpen = () => {
+    setForgotActive(!forgotActive)
+    setLoginActiv(false)
+    setSignUpActiv(false)
+    setVerifyCodeActive(false)
+  }
+
+  const VerifyCodeOpen = () => {
+    setVerifyCodeActive(!verifyCodeActive)
+    setLoginActiv(false)
+    setSignUpActiv(false)
+    setForgotActive(false)
+  }
+
+  const NewPassOpen = () => {
+    console.log(newPassActive, 'newPassActive')
+    setNewPassActive(!newPassActive)
+    setLoginActiv(false)
+    setSignUpActiv(false)
+    setForgotActive(false)
+    setVerifyCodeActive(false)
+  }
+
+  let setLocalStorage = (token) => {
+    setToken(token)
+    console.log('aaapopp')
+  }
+
+  const bbb = () => {
+    setLoginActiv(false)
+  }
+
+  let getResponse = (userId, token) => {
+    setRes({ userId, token })
+  }
+
+  function checkoutPageRedirect() {
+    if (!token) {
+      loginOpen();
+    }
+    else if (!delAddresses || !verified) {
+      setAlert(true);
+    } else {
+      setAlert(false);
+      history.push("/checkout");
+    }
   }
 
   return (
@@ -53,7 +119,7 @@ function ProductsDialog() {
       {shoppingCartItems.map((cartItem) => (
         <CartItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      {token && products.length ? (
+      {products.length ? (
         <button
           onClick={checkoutPageRedirect}
           className="BestSavers__link BestSavers__link__card"
@@ -74,6 +140,39 @@ function ProductsDialog() {
           </Alert>
         )}
       </Link>
+      <Login
+        aaa={bbb}
+        loginActiv={loginActiv}
+        loginOpen={loginOpen}
+        signUpActiv={signUpActiv}
+        SignUpOpen={SignUpOpen}
+        ForgotPassOpen={ForgotPassOpen}
+        localToken={setLocalStorage}
+      />
+      <SignUp
+        aaa={bbb}
+        loginActiv={loginActiv}
+        loginOpen={loginOpen}
+        signUpActiv={signUpActiv}
+        SignUpOpen={SignUpOpen}
+        localToken={setLocalStorage}
+      />
+      <ForgotPass
+        forgotActive={forgotActive}
+        ForgotPassOpen={ForgotPassOpen}
+        VerifyCodeOpen={VerifyCodeOpen}
+      />
+      <NewPass
+        newPassActive={newPassActive}
+        NewPassOpen={NewPassOpen}
+        res={res}
+      />
+      <VerifyCode
+        verifyCodeActive={verifyCodeActive}
+        VerifyCodeOpen={VerifyCodeOpen}
+        NewPassOpen={NewPassOpen}
+        getResponse={getResponse}
+      />
     </>
   );
 }
