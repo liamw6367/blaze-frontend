460a133d8e5dda65c66a7663e4d87943dee18a03
Miss-Coder-developer <misscoderexpert@gmail.com>
2021-11-11 11:58:13 +0400

    test

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -105,7 +105,7 @@ const CartItem = ({ cartItem }) => {
             );
           })
         }
-                {
+        {
           product_stores?.map(productStore => {
             return (
               <div className="phoneNum">
